4a96ecad05cef46df5950e7b4f657dd6c9c2dbac
umesh <user@aravindansurendran1.mylabserver.com>
2018-05-29 04:29:13 +0000

    adding learn cookbook

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -3,7 +3,13 @@
 current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
+<<<<<<< HEAD
 node_name                "umesh"
 client_key               "#{current_dir}/umesh.pem"
 chef_server_url          "https://aravindansurendran3.mylabserver.com/organizations/linuxacademy"
+=======
+node_name                "aravindan"
+client_key               "#{current_dir}/aravindan.pem"
+chef_server_url          "https://aravindansurendran2.mylabserver.com/organizations/linuxacademy"
+>>>>>>> 70488e5bd0198c142dbe5a9242f1b72a75c93c15
 cookbook_path            ["#{current_dir}/../cookbooks"]
--- roles/starter.rb
+++ roles/starter.rb
@@ -2,5 +2,9 @@ name "starter"
 description "An example Chef role"
 run_list "recipe[starter]"
 override_attributes({
+<<<<<<< HEAD
   "starter_name" => "umesh kanagaraj",
+=======
+  "starter_name" => "aravindan surendran",
+>>>>>>> 70488e5bd0198c142dbe5a9242f1b72a75c93c15
 })
